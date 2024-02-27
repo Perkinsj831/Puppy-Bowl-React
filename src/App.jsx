c0e6418729b27d38eb513958b054623b1e807312
John Perkins <mrgreen@MrGreens-Laptop.local>
2024-02-27 10:11:13 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import SinglePlayer from './components/SinglePlayer'
+import AllPlayers from './components/AllPlayers'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
